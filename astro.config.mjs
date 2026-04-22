48aaf9d1ad70672b56dd5051f61e701917a5eed6
Weston <westonkelliher@gmail.com>
2026-04-22 14:45:34 -0600

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: process.env.CF ? cloudflare() : undefined,
 });
